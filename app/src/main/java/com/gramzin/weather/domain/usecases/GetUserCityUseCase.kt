348600dabfff820882b7cf6d1a7f28ab6dc2aefb
Aleksei Gramzin <gramzin4666@gmail.com>
2023-08-23 15:32:05 +0300

    Added functionality to GetUserCityUseCase

--- app/src/main/java/com/gramzin/weather/domain/usecases/GetUserCityUseCase.kt
+++ app/src/main/java/com/gramzin/weather/domain/usecases/GetUserCityUseCase.kt
@@ -1,4 +1,14 @@
 package com.gramzin.weather.domain.usecases
 
-class GetUserCityUseCase {
+import com.gramzin.weather.domain.model.City
+import com.gramzin.weather.domain.repository.CityRepository
+import io.reactivex.rxjava3.core.Single
+
+class GetUserCityUseCase(
+    private val cityRepository: CityRepository
+) {
+
+    operator fun invoke(latitude: Double, longitude: Double): Single<City> {
+        return cityRepository.getCurrentCityName(latitude, longitude)
+    }
 }
