dc8731b6e6197e997d9ece9d56fc3373459ae2bf
Aleksei Gramzin <gramzin4666@gmail.com>
2023-08-27 19:38:04 +0300

    Added additional fields to the WeatherType constructor

--- app/src/main/java/com/gramzin/weather/domain/WeatherType.kt
+++ app/src/main/java/com/gramzin/weather/domain/WeatherType.kt
@@ -1,14 +1,56 @@
 package com.gramzin.weather.domain
 
-sealed interface WeatherType{
+import com.gramzin.weather.R
 
-    object ClearSky: WeatherType
-    object FewClouds: WeatherType
-    object ScatteredClouds: WeatherType
-    object BrokenClouds: WeatherType
-    object ShowerRain: WeatherType
-    object Rain: WeatherType
-    object ThunderStorm: WeatherType
-    object Snow: WeatherType
-    object Mist: WeatherType
+sealed class WeatherType(
+    val titleResId: Int,
+    val dayIconResId: Int,
+    val nightIconResId: Int
+){
+
+    object ClearSky: WeatherType(
+        R.string.clear_sky,
+        R.drawable.clear_sky_day,
+        R.drawable.clear_sky_night
+    )
+    object FewClouds: WeatherType(
+        R.string.few_clouds,
+        R.drawable.few_clouds_day,
+        R.drawable.few_clouds_night
+    )
+    object ScatteredClouds: WeatherType(
+        R.string.scattered_clouds,
+        R.drawable.few_clouds_day,
+        R.drawable.few_clouds_night
+    )
+    object BrokenClouds: WeatherType(
+        R.string.broken_clouds,
+        R.drawable.clouds,
+        R.drawable.clouds
+    )
+    object ShowerRain: WeatherType(
+        R.string.shower_rain,
+        R.drawable.shower_rain,
+        R.drawable.shower_rain
+    )
+    object Rain: WeatherType(
+        R.string.rain,
+        R.drawable.rain_day,
+        R.drawable.rain_night
+    )
+    object ThunderStorm: WeatherType(
+        R.string.thunderstorm,
+        R.drawable.thunderstorm,
+        R.drawable.thunderstorm
+    )
+    object Snow: WeatherType(
+        R.string.snow,
+        R.drawable.snow,
+        R.drawable.snow
+    )
+    object Mist: WeatherType(
+        R.string.mist,
+        R.drawable.clouds,
+        R.drawable.clouds
+    )
 }
